f75862aa90dcd26d6ab77b751ea61e92fdbd11bb
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-08-30 23:28:24 -0600

    Create Icons RS and add to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 import React from "react"
 import Logo from "./Logo"
+import {TwitterIcon, GithubIcon, LinkedInIcon } from "./Icons"
+import {motion} from "framer-motion";
 
 const CustomLink = ({href, title, className=""}) => {
+    const router = useRouter();
+    
     return(
         <Link href={href} className={`${className} relative group`}>
             {title}
-            <span className="h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">&nbsp;</span>
+            <span className={`h-[1px] inline-block bg-dark 
+            absolute left-0 -bottom-0.5 group-hover:w-full 
+            transition-[width] ease duration-300 
+            ${router.asPath === href ? 'w-full':'w-0'}`}>&nbsp;</span>
         </Link>
     )
 }
@@ -20,13 +28,11 @@ const NavBar = () => {
                 <CustomLink href="/projects" title="Projects" className="mr-4"/>
                 <CustomLink href="/articles" title="Articles" className="ml-4"/>
             </nav>
-            <nav>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
+            <nav className="flex items-center justify-center flex-wrap">
+                <motion.a href="https://github.com/Alejandrozf23" target={"_blank"} 
+                    whileHover={{y:-3}} whileTap={{scale:0.9}} className="w-6 mr-3"><GithubIcon/></motion.a>
+                <motion.a href="https://www.linkedin.com/in/alejandro-de-jesús-zepeda-flores-5251161b6" target={"_blank"}
+                    whileHover={{y:-3}} whileTap={{scale:0.9}} className="w-6 mx-3"><LinkedInIcon/></motion.a>
             </nav>
             <div className="absolute left-[50%] top-2 translate-x-[-50%]">
                 <Logo/>
