beec182b9742438e7db7d34e341ee1c81b71a377
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-09-01 16:58:27 -0600

    Fix image developer pixelation

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,12 @@ export default function Home() {
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
             <div className='w-1/2'>
-              <Image src={profilePic} alt="CodeBucks" className="w-full h-auto"/>
+              <Image src={profilePic} alt="CodeBucks" className='w-full h-auto'/>
             </div>
             <div className='w-1/2'>
               <h1>Turning Vision Into Reality With Code And Design.</h1>
-              <p1>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-                  Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p1>
+              <p>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
+                Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
             </div>
           </div>
         </Layout>
