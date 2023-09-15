432a0ebe6d59035f9bcb1e66f804b56af0cf4d46
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-09-15 16:54:19 -0600

    Add button with icon to download pdf cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/developer-pic-1.png"
 import Link from 'next/link'
+import { LinkArrow } from '@/components/Icons'
 
 export default function Home() {
   return (
@@ -20,11 +21,12 @@ export default function Home() {
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Hidden Services Architect? Backend Developer" className='!text-6x1'/>
-              <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-                Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
-              <div>
-                <Link href="/dummy.pdf">Resume</Link>
-                <Link href="mailto:alexzepeda2908@gmail.com">Contact</Link>
+              <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web 
+                applications. Explore my latest projects and articles, showcasing my expertise in Spring, Django y React.js and web development.</p>
+              <div className='flex items-center self-start mt-2'>
+                <Link href="/dummy.pdf" target={"_blank"} className='flex items-center bg-dark text-light 
+                  p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 
+                  border-solid border-transparent hover:border-dark' download={true}>Resume <LinkArrow className={"w-6 ml-1"}/></Link>
               </div>
             </div>            
           </div>
