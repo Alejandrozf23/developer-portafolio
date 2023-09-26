b59a74101127e5fb9da103802349e77be85af29d
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-09-25 20:36:57 -0600

    Add shadow to cards projects with 3D effect

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,8 +9,9 @@ import Image from "next/image";
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
     return(
-        <article className="w-full flex items-center justify-between rounded-3xl 
+        <article className="w-full flex items-center justify-between relative rounded-br-2xl rounded-3xl 
             border border-solid border-dark bg-light shadow-2xl p-12">
+            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl"/>                
             <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
                 <Image src={img} alt={title} className="w-full h-auto"/>
             </Link>
@@ -33,6 +34,7 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
 const Project = ({title, type, img, link, github}) => {
     return(
         <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl"/>
             <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
                 <Image src={img} alt={title} className="w-full h-auto"/>
             </Link>
