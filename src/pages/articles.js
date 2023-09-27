41b552f218dadd6932ec0d69f27d9fd06b4651c3
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-09-27 15:00:05 -0600

    Create Featured article component

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,7 +1,25 @@
 import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import Link from "next/link";
 import React from "react";
+import article1 from "../../public/images/articles/pagination component in reactjs.jpg"
+import Image from "next/image";
+
+const FeaturedArticle = ({img, title, time, summary, link}) => {
+    return(
+        <li>
+            <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
+                <Image src={img} alt={title} className="w-full h-auto"/>
+            </Link>
+            <Link href={link} target="_blank">
+                <h2 className="capitalize text-2x1 font-bold my-2">{title}</h2>
+            </Link>
+            <p className="text-sm mb-2">{summary}</p>
+            <span className="text-primary font-semibold">{time}</span>
+        </li>
+    )
+}
 
 const articles = () => {
     return (
@@ -14,8 +32,14 @@ const articles = () => {
                 <Layout className="pt-16">
                     <AnimatedText text="Words can change the world!" className="mb-16"/>
                     <ul className="grid grid-cols-2 gap-16">
-                        <li>Featured articles-1</li>
-                        <li>Featured articles-2</li>
+                        <FeaturedArticle 
+                        title="Build A Custom Pagination Component In Reactjs From Scratch"
+                        summary="Learn how to build a custom pagination component in ReactJS from scratch. 
+                        Follow this step-by-step guide to integrate Pagination component in your ReactJS project.
+                        9 min read"
+                        time="9 min read"
+                        link="/"
+                        img={article1}/>
                     </ul>
                 </Layout>
             </main>
