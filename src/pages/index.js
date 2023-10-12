45d3b7f97e35605084807597a5ac4f9ec14fc3c7
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-11 22:55:42 -0600

    Make it responsive HireMe button and logo NavBar

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,17 +16,17 @@ export default function Home() {
       </Head>
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
         <Layout className='pt-0 md:p-16 sm:pt-8'>
-          <div className='flex items-center justify-between w-full'>
-            <div className='w-1/2'>
-              <Image src={profilePic} alt="CodeBucks" className='w-full h-auto' 
+          <div className='flex items-center justify-between w-full lg:flex-col'>
+            <div className='w-1/2 md:w-full'>
+              <Image src={profilePic} alt="CodeBucks" className='w-full h-auto lg:hidden md:inline-block md:w-full' 
                 priority sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'/>
             </div>
-            <div className='w-1/2 flex flex-col items-center self-center'>
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText text="Hidden Services Architect? Backend Developer" 
-                className='!text-6x1  xl:!text-5x1 lg:!text-center lg:!text=6x1 md:!text-5x1 sm:!text-3x1'/>
+                className='!text-6x1 xl:!text-5x1 lg:!text-center lg:!text=6x1 md:!text-5x1 sm:!text-3x1'/>
               <p className='my-4 text-base font-medium '>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web 
                 applications. Explore my latest projects and articles, showcasing my expertise in Spring, Django y React.js and web development.</p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/dummy.pdf" target={"_blank"} className='flex items-center bg-dark text-light 
                   p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 
                   border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark 
