e1d16a802e2f61dfac13b2a2242db9b2ef6455ec
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-12 20:29:26 -0600

    Make it responsive experience & education in about page

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,20 +4,22 @@ import LiIcon from "./LiIcon";
 
 const Details = ({position, company, companyLink, time, address, work}) => {
     const ref = useRef(null);
-    return <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
-        <LiIcon reference={ref}/>
-        <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
-            <h3 className="capitalize font-bold text-2xl">{position}&nbsp;
-                <a href={companyLink} target="_blank" className="text-primary capitalize dark:text-primaryDark">@{company}</a>
-            </h3>
-            <span className="capitalize font-medium text-dark/75 dark:text-light/75">
-                {time} | {address}
-            </span>
-            <p className="font-medium w-full">
-                {work}
-            </p>
-        </motion.div>
-    </li>
+    return (
+        <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
+            <LiIcon reference={ref}/>
+            <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
+                <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">{position}&nbsp;
+                    <a href={companyLink} target="_blank" className="text-primary capitalize dark:text-primaryDark">@{company}</a>
+                </h3>
+                <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
+                    {time} | {address}
+                </span>
+                <p className="font-medium w-full md:text-sm">
+                    {work}
+                </p>
+            </motion.div>
+        </li>
+    );
 }
 
 const Experience = () => {
@@ -31,10 +33,10 @@ const Experience = () => {
 
     return(
         <div className="my-64">
-            <h2 className="font-bold text-8x1 mb-32 w-full text-center">Experience</h2>
-            <div ref={ref} className="w-[75%] mx-auto relative">
-                <motion.div style={{scaleY:scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"/>
-                <ul className="w-full flex flex-col items-start justify-between ml-4">
+            <h2 className="font-bold text-8x1 mb-32 w-full text-center md:text-6x1 xs:text-4x1 md:mb-16">Experience</h2>
+            <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
+                <motion.div style={{scaleY:scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"/>
+                <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
                     <Details position="Software Engineer" company="B1Soft Latinoamerica" time="2020-2021" address="Mexico City" companyLink="https://b1-soft.com:449/bienvenido/"
                         work="Design and implementation of views for web applications related to electronic invoicing and 
                             purchase of licensing plans and develop a web application to monitor the stamping process of the 
