8c66a63ae0f507a59888ee3e53e8dc0ad204524e
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-23 21:22:59 -0600

    Create constants to index page

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+const data = {
+  head: {
+    title: 'Adejesuszf | Backend developer',
+    meta: {
+      name: 'description',
+      content: 'Web Portfolio of a Backend Developer',
+    },
+  },
+  profile: {
+    picture: '/profile/developer01.png',
+  },
+  info: {
+    title: 'Hidden Services Architect? Backend Developer',
+    description: 'As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in Spring, Django y React.js and web development',
+    pdf: 'pdf/dummy.pdf',
+    name: 'Resume',
+  }
+}
+
+export default data
