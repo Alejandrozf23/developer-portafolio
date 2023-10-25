9e739cf68d94cdc2d3e65af43b6aa294ba9c0176
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-24 22:53:59 -0600

    Create constants to about page

---
+++ src/constants/about.js
@@ -0,0 +1,51 @@
+const data = {
+    head: {
+      title: 'Adejesuszf | About',
+      meta: {
+        name: 'description',
+        content: 'Explore my journey as a programmer, where I share my technical skills, professional experience and academic training.Web Portfolio of a Backend Developer',
+      },
+    },
+    about: {
+        title: 'Passion Fuels Purpose!',
+        biography: {
+            title:'Biography',
+            description: [
+                {
+                    id: '001',
+                    paragraph: '- Hi, I\'m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and innovative ways to bring my clients\' visions to life.',
+                    className: 'font-medium'
+                },
+                {
+                    id: '002',
+                    paragraph: '- I believe that design is about more than just making things look pretty – it\'s about solving problems and creating intuitive, enjoyable experiences for users.',
+                    className: 'my-4 font-medium'
+                },
+                {
+                    id: '003',
+                    paragraph: '- Whether I\'m working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.',
+                    className: 'font-medium'
+                },
+            ],
+            experience: [
+                {
+                    number: 50,
+                    text: '+',
+                    description: 'satisfied clients!'
+                },
+                {
+                    number: 40,
+                    text: '+',
+                    description: 'projects completed'
+                },
+                {
+                    number: 4,
+                    text: '+',
+                    description: 'years of experience'
+                },
+            ]
+        },
+    },
+}
+  
+export default data
