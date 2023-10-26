bc439bea5be493063775243ac53b3c43d1475d25
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-25 21:51:56 -0600

    Create constants to skills & modify to read constants

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,69 +1,34 @@
 import React from 'react';
-import Image from "next/image";
+import Image from 'next/image'
+import data from '@/constants/skills'
 import FastMarquee from 'react-fast-marquee';
-import angular from "@/images/skills/Angular.png"
-import spring from "@/images/skills/Spring.png"
-import c from "@/images/skills/C++.png"
-import firebase from "@/images/skills/Firebase.png"
-import java from "@/images/skills/Java.png"
-import javascript from "@/images/skills/Javascript.png"
-import django from "@/images/skills/Django.png"
-import php from "@/images/skills/PHP.png"
-import python from "@/images/skills/Python.png"
-import react from "@/images/skills/React.png"
-import tailwind from "@/images/skills/Tailwind.png"
-import nextjs from "@/images/skills/Nextjs.png"
-import oracle from "@/images/skills/Oracle.png"
-import mongodb from "@/images/skills/Mongo.png"
-import redis from "@/images/skills/Redis.png"
-import docker from "@/images/skills/Docker.png"
-import kubernates from "@/images/skills/Kubernates.png"
-import kafka from "@/images/skills/Kafka.png"
-import confluent from "@/images/skills/Confluent.png"
-import aws from "@/images/skills/AWS.png"
 
-
-const ImageSkills = ({src}) => {
+const ImageSkills = ({src, name}) => {
     return (
-        <Image src={src} alt={src} width={100} height={100}
-            className='rounded-2xl hover:scale-110 cursor-pointer 
-            transition-all duration-300 ease-in-out object-cover object-center'/>
+        <div key={name} className='flex w-full flex-col items-center justify-center'>
+            <Image src={src} alt={name} width={100} height={100}
+                className='rounded-2xl hover:scale-110 cursor-pointer 
+                transition-all duration-300 ease-in-out object-cover object-center'/>
+        </div>
     );
 }
 
 const Skills = () => {
+    const skillList = data.skills;
+
     return(
         <>            
             <div className="my-32">
                 <h2 className="font-bold text-8x1 mb-8 w-full text-center md:text-6x1 xs:text-4x1 md:mb-4">Tech Skills</h2>
                 <div className='mt-0'>
-                    <FastMarquee speed={75} direction="right" gradientColor={['red', 'blue']}>
+                    <FastMarquee speed={80} direction="right" gradientColor={['red', 'blue']}>
                         <div className='flex items-center xs:gap-[50px] gap-[80px] xs:mr-[50px] mr-[80px] w-auto'>
-                            <ImageSkills src={java}/>
-                            <ImageSkills src={python}/>
-                            <ImageSkills src={c}/>
-                            <ImageSkills src={php}/>
-                            <ImageSkills src={javascript}/>
-
-                            <ImageSkills src={angular}/>
-                            <ImageSkills src={react}/>
-                            <ImageSkills src={tailwind}/>
-                            <ImageSkills src={nextjs}/>
-
-                            <ImageSkills src={spring}/>
-                            <ImageSkills src={django}/>
-                            
-                            <ImageSkills src={oracle}/>
-                            <ImageSkills src={firebase}/>
-                            <ImageSkills src={mongodb}/>
-                            <ImageSkills src={redis}/>
-
-                            <ImageSkills src={docker}/>
-                            <ImageSkills src={kubernates}/>
-                            <ImageSkills src={kafka}/>
-                            <ImageSkills src={confluent}/>
-
-                            <ImageSkills src={aws}/>                            
+                        {
+                            skillList.map((detail) => (
+                                <ImageSkills src={detail.link} name={detail.name}/>
+                            ))
+                        }
+                                                        
                         </div>
                     </FastMarquee>
                 </div>
---
+++ src/constants/skills.js
@@ -0,0 +1,82 @@
+const data = {
+    skills: [
+        {
+            name: 'JAVA',
+            link: '/images/skills/Java.png'
+        },
+        {
+            name: 'PYTHON',
+            link: '/images/skills/Python.png'
+        },
+        {
+            name: 'C',
+            link: '/images/skills/C++.png'
+        },
+        {
+            name: 'PHP',
+            link: '/images/skills/PHP.png'
+        },
+        {
+            name: 'JAVASCRIPT',
+            link: '/images/skills/Javascript.png'
+        },
+        {
+            name: 'ANGULAR',
+            link: '/images/skills/Angular.png'
+        },
+        {
+            name: 'REACT',
+            link: '/images/skills/React.png'
+        },
+        {
+            name: 'TAILWIND',
+            link: '/images/skills/Tailwind.png'
+        },
+        {
+            name: 'NEXT JS',
+            link: '/images/skills/Nextjs.png'
+        },
+        {
+            name: 'SPRING',
+            link: '/images/skills/Spring.png'
+        },
+        {
+            name: 'DJANGO',
+            link: '/images/skills/Django.png'
+        },
+        {
+            name: 'ORACLE',
+            link: '/images/skills/Oracle.png'
+        },
+        {
+            name: 'FIREBASE',
+            link: '/images/skills/Firebase.png'
+        },
+        {
+            name: 'MONGODB',
+            link: '/images/skills/Mongo.png'
+        },
+        {
+            name: 'REDIS',
+            link: '/images/skills/Redis.png'
+        },
+        {
+            name: 'DOCKER',
+            link: '/images/skills/Docker.png'
+        },
+        {
+            name: 'KUBERNATES',
+            link: '/images/skills/Kubernates.png'
+        },
+        {
+            name: 'KAFKA',
+            link: '/images/skills/Kafka.png'
+        },
+        {
+            name: 'CONFLUENT',
+            link: '/images/skills/Confluent.png'
+        },         
+    ]
+}
+
+export default data
