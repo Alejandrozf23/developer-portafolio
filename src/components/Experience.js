0c2badb65e80f956ce886c6951db10161ee6f185
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-25 21:02:38 -0600

    Create constants to experience & modify to read constants

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
-import { motion, useScroll } from "framer-motion";
-import LiIcon from "./LiIcon";
+import LiIcon from './LiIcon'
+import data from '@/constants/experience'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion'
 
 const Details = ({position, company, companyLink, time, address, work}) => {
     const ref = useRef(null);
@@ -31,26 +32,21 @@ const Experience = () => {
         }
     );
 
+    const experienceList = data.experience;
+
     return(
         <div className="my-32">
             <h2 className="font-bold text-8x1 mb-8 w-full text-center md:text-6x1 xs:text-4x1 md:mb-4">Experience</h2>
             <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
-                <motion.div style={{scaleY:scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"/>
+                <motion.div style={{scaleY:scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full 
+                    bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"/>
                 <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
-                    <Details position="Software Engineer" company="B1Soft Latinoamerica" time="2020-2021" address="Mexico City" companyLink="https://b1-soft.com:449/bienvenido/"
-                        work="Design and implementation of views for web applications related to electronic invoicing and 
-                            purchase of licensing plans and develop a web application to monitor the stamping process of the 
-                            companies showing in detail the steps of said process."/>
-                    <Details position="Backend Developer" company="Banco Azteca" time="2021-2023" address="Mexico City" companyLink="https://www.bancoazteca.com.mx/"
-                        work="Design and implementation of libraries for both SQL and NoSQL database connection utilities,
-                            Kafka publishers and consumers and develop microservices for propagable events with the premise 
-                            of event-oriented distribution. Implementation of event-oriented architecture with the purpose 
-                            of triggering and establishing communication between decoupled microservices."/>
-                    <Details position="Backend Developer" company="Bluetab America" time="2023-Present" address="Mexico City" companyLink="https://www.bluetab.net/es/"
-                        work="Design and implementation of libraries for both SQL and NoSQL database connection utilities,
-                            Kafka publishers and consumers and develop microservices for propagable events with the premise 
-                            of event-oriented distribution. Implementation of event-oriented architecture with the purpose 
-                            of triggering and establishing communication between decoupled microservices."/>
+                    {
+                        experienceList.map((detail) => (
+                            <Details position={detail.position} company={detail.company} time={detail.time} 
+                                address={detail.address} companyLink={detail.companyLink} work={detail.work}/>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
---
+++ src/constants/experience.js
@@ -0,0 +1,30 @@
+const data = {
+    experience: [
+        {
+            position: 'Software Engineer',
+            company: 'B1Soft Latinoamerica',
+            time: '2020-2021',
+            address: 'Mexico City',
+            companyLink: 'https://b1-soft.com:449/bienvenido/',
+            work: 'Design and implementation of views for web applications related to electronic invoicing and purchase of licensing plans and develop a web application to monitor the stamping process of the companies showing in detail the steps of said process.'
+        },
+        {
+            position: 'Backend Developer',
+            company: 'Banco Azteca',
+            time: '2021-2023',
+            address: 'Mexico City',
+            companyLink: 'https://www.bancoazteca.com.mx/',
+            work: 'Design and implementation of libraries for both SQL and NoSQL database connection utilities, Kafka publishers and consumers and develop microservices for propagable events with the premise of event-oriented distribution. Implementation of event-oriented architecture with the purpose of triggering and establishing communication between decoupled microservices.'
+        },
+        {
+            position: 'Backend Developer',
+            company: 'Bluetab America',
+            time: '2023-Present',
+            address: 'Mexico City',
+            companyLink: 'https://www.bluetab.net/es/',
+            work: 'Design and implementation of libraries for both SQL and NoSQL database connection utilities, Kafka publishers and consumers and develop microservices for propagable events with the premise of event-oriented distribution. Implementation of event-oriented architecture with the purpose of triggering and establishing communication between decoupled microservices.'
+        },
+    ]
+}
+
+export default data
