c13ac00173bce20fa7a5b7665d35456ccea4fc1d
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-10-29 21:18:28 -0600

    Create constants to articles page

---
+++ src/constants/articles.js
@@ -0,0 +1,48 @@
+const data = {
+    articles: [
+        {
+            id: 'A-001',
+            feature: true,
+            title: 'Build A Custom Pagination Component In Reactjs From Scratch',
+            summary: 'Learn how to build a custom pagination component in ReactJS from scratch. Follow this step-by-step guide to integrate Pagination component in your ReactJS project.',
+            time: '9 min read',
+            link: '/',
+            img: '/images/articles/pagination component in reactjs.jpg'
+        },
+        {
+            id: 'A-002',
+            feature: true,
+            title: 'Build A Custom Pagination Component In Reactjs From Scratch',
+            summary: 'Learn how to build a custom pagination component in ReactJS from scratch. Follow this step-by-step guide to integrate Pagination component in your ReactJS project.',
+            time: '9 min read',
+            link: '/',
+            img: '/images/articles/create loading screen in react js.jpg'
+        },
+        {
+            id: 'A-003',
+            feature: false,
+            title: 'Form Validation In Reactsjs: Build a reusable custom hook for inputs and error handling',
+            date: 'July 13, 2023',
+            link: '/',
+            img: '/images/articles/create modal component in react using react portals.png'
+        },
+        {
+            id: 'A-004',
+            feature: false,
+            title: 'Form Validation In Reactsjs: Build a reusable custom hook for inputs and error handling',
+            date: 'July 13, 2023',
+            link: '/',
+            img: '/images/articles/create modal component in react using react portals.png'
+        },
+        {
+            id: 'A-005',
+            feature: false,
+            title: 'Form Validation In Reactsjs: Build a reusable custom hook for inputs and error handling',
+            date: 'July 13, 2023',
+            link: '/',
+            img: '/images/articles/create modal component in react using react portals.png'
+        },
+    ]
+}
+
+export default data
