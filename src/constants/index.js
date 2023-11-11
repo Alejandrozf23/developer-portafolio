e974b244f2fcca4d5fbd1d33a93f6687a8553845
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-11-10 20:32:31 -0600

    Add pdf CV and update path in resume button

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ const data = {
   info: {
     title: 'Hidden Services Architect? Backend Developer',
     description: 'As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in Spring, Django y React.js and web development',
-    pdf: 'pdf/dummy.pdf',
+    pdf: 'pdf/CV Adejesuszf ENG.pdf',
     name: 'Resume',
   }
 }
