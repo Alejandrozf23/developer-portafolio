a854c306da57427cecad9624bb35318fca1b9504
Alejandro Zepeda <alexzepeda07@hotmail.com>
2023-11-10 20:26:53 -0600

    Delete projects, only two examples

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -20,43 +20,6 @@ const data = {
             type: 'Featured Project',
             img: '/images/projects/crypto-screener-cover-image.jpg',
         },
-        {
-            id: 'P-003',
-            feature: false,
-            title: 'Crypto Screener Application',            
-            link: '/',
-            github: '/',
-            type: 'Featured Project',
-            img: '/images/projects/devdreaming.jpg',
-        },
-        {
-            id: 'P-004',
-            feature: true,
-            title: 'Crypto Screener Application',
-            summary: 'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.',
-            link: '/',
-            github: '/',
-            type: 'Featured Project',
-            img: '/images/projects/fashion-studio-website.jpg',
-        },
-        {
-            id: 'P-005',
-            feature: false,
-            title: 'Crypto Screener Application',    
-            link: '/',
-            github: '/',
-            type: 'Featured Project',
-            img: '/images/projects/nft-collection-website-cover-image.jpg',
-        },
-        {
-            id: 'P-006',
-            feature: false,
-            title: 'Crypto Screener Application',            
-            link: '/',
-            github: '/',
-            type: 'Featured Project',
-            img: '/images/projects/portfolio-cover-image.jpg',
-        },
     ]
 }
 
